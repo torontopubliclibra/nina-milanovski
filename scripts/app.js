5a349272133dbe2aa5baf94f268536f89eeea22c
torontopubliclibra <dana.r.teagle@gmail.com>
2024-02-29 11:44:51 -0500

    feat: refactored image sliders to handle 4 different projects

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,61 @@
 // app object
 let app = {
 
-    currentSlide: 0,
-    maxSlide: 0,
-
     // page elements
     elements: {
         body: $("body"),
         nav: $("nav"),
-        slides: document.querySelectorAll(".img-slide"),
-        slidetext: document.querySelectorAll(".img-text"),
-        nextSlide: document.querySelector(".next"),
-        prevSlide: document.querySelector(".prev"),
+    },
+
+    project_1: {
+        id: 1,
+        currentSlide: 0,
+        maxSlide: 0,
+        slides: document.querySelectorAll(".project-1 .img-slide"),
+        slidetext: document.querySelectorAll(".project-1 .img-text"),
+        nextSlide: document.querySelector(".project-1 .img-textbox .img-nav.next"),
+        prevSlide: document.querySelector(".project-1 .img-textbox .img-nav.prev"),
+    },
+
+    project_2: {
+        id: 2,
+        currentSlide: 0,
+        maxSlide: 0,
+        slides: document.querySelectorAll(".project-2 .img-slide"),
+        slidetext: document.querySelectorAll(".project-2 .img-text"),
+        nextSlide: document.querySelector(".project-2 .img-textbox .img-nav.next"),
+        prevSlide: document.querySelector(".project-2 .img-textbox .img-nav.prev"),
+    },
+    project_3: {
+        id: 3,
+        currentSlide: 0,
+        maxSlide: 0,
+        slides: document.querySelectorAll(".project-3 .img-slide"),
+        slidetext: document.querySelectorAll(".project-3 .img-text"),
+        nextSlide: document.querySelector(".project-3 .img-textbox .img-nav.next"),
+        prevSlide: document.querySelector(".project-3 .img-textbox .img-nav.prev"),
+    },
+    project_4: {
+        id: 4,
+        currentSlide: 0,
+        maxSlide: 0,
+        slides: document.querySelectorAll(".project-4 .img-slide"),
+        slidetext: document.querySelectorAll(".project-4 .img-text"),
+        nextSlide: document.querySelector(".project-4 .img-textbox .img-nav.next"),
+        prevSlide: document.querySelector(".project-4 .img-textbox .img-nav.prev"),
     },
 
     // app functions
     functions: {
 
         // change image carousel slide text function
-        changeSlideText: () => {
+        changeSlideText: (id) => {
 
             // loop through all the slide text
-            app.elements.slidetext.forEach((text, index) => {
+            app[`project_${id}`].slidetext.forEach((text, index) => {
 
                 // if the text matches the currently displayed image
-                if (index === app.currentSlide){
+                if (index === app[`project_${id}`].currentSlide){
 
                     // display that text
                     text.style.display = `initial`;
@@ -37,56 +68,56 @@ let app = {
         },
 
         // image carousel previous slide button function
-        prevSlideFunction: () => {
+        prevSlideFunction: (id) => {
 
             // if the current slide is the first
-            if (app.currentSlide === 0) {
+            if (app[`project_${id}`].currentSlide === 0) {
 
                 // change it to the last slide
-                app.currentSlide = app.maxSlide;
+                app[`project_${id}`].currentSlide = app[`project_${id}`].maxSlide;
             } else {
 
                 // otherwise move back one slide
-                app.currentSlide--;
+                app[`project_${id}`].currentSlide--;
             }
 
             // loop through image carousel slide images
-            app.elements.slides.forEach((slide, index) => {
+            app[`project_${id}`].slides.forEach((slide, index) => {
 
                 // move them all back by one slide
-                slide.style.transform = `translateX(${100 * (index - app.currentSlide)}%)`;
+                slide.style.transform = `translateX(${100 * (index - app[`project_${id}`].currentSlide)}%)`;
             });
 
             // update slide text to current slide
-            app.functions.changeSlideText();
+            app.functions.changeSlideText(id);
 
         },
 
         // image carousel next slide button function
-        nextSlideFunction: () => {
+        nextSlideFunction: (id) => {
 
             // if the current slide is the last
-            if (app.currentSlide === app.maxSlide) {
+            if (app[`project_${id}`].currentSlide === app[`project_${id}`].maxSlide) {
 
                 // change it to the first slide
-                app.currentSlide = 0;
+                app[`project_${id}`].currentSlide = 0;
 
             } else {
 
                 // otherwise move forward one slide
-                app.currentSlide++;
+                app[`project_${id}`].currentSlide++;
             }
 
             // loop through image carousel slide images
-            app.elements.slides.forEach((slide, index) => {
+            app[`project_${id}`].slides.forEach((slide, index) => {
 
                 // move them all forward by one slide
-                slide.style.transform = `translateX(${100 * (index - app.currentSlide)}%)`;
+                slide.style.transform = `translateX(${100 * (index - app[`project_${id}`].currentSlide)}%)`;
 
             });
 
             // update slide text to current slide
-            app.functions.changeSlideText();
+            app.functions.changeSlideText(id);
 
         },
 
@@ -103,26 +134,68 @@ let app = {
         // on the nav button click, toggle the nav
         app.elements.nav.click(app.functions.toggleNav);
 
-        // add event listener to image carousel previous slide button
-        if (app.elements.prevSlide) {
+        app.project_1.prevSlide.addEventListener('click', () => app.functions.prevSlideFunction(1));
+        app.project_2.prevSlide.addEventListener('click', () => app.functions.prevSlideFunction(2));
+        app.project_3.prevSlide.addEventListener('click', () => app.functions.prevSlideFunction(3));
+        app.project_4.prevSlide.addEventListener('click', () => app.functions.prevSlideFunction(4));
 
-            app.elements.prevSlide.addEventListener('click', app.functions.prevSlideFunction);
-        }
-
-        // add event listener to image carousel next slide button
-        if (app.elements.nextSlide) {
-            app.elements.nextSlide.addEventListener('click', app.functions.nextSlideFunction);
-        }
+        app.project_1.nextSlide.addEventListener('click', () => app.functions.nextSlideFunction(1));
+        app.project_2.nextSlide.addEventListener('click', () => app.functions.nextSlideFunction(2));
+        app.project_3.nextSlide.addEventListener('click', () => app.functions.nextSlideFunction(3));
+        app.project_4.nextSlide.addEventListener('click', () => app.functions.nextSlideFunction(4));
 
     },
     
     // app initializion
     init: () => {
 
-        app.currentSlide = 0,
-        app.maxSlide = app.elements.slides.length - 1,
+        app.project_1.currentSlide = 0,
+        app.project_2.currentSlide = 0,
+        app.project_3.currentSlide = 0,
+        app.project_4.currentSlide = 0,
 
-        app.elements.slides.forEach((slide, index) => {
+        app.project_1.maxSlide = app.project_1.slides.length - 1,
+        app.project_2.maxSlide = app.project_2.slides.length - 1,
+        app.project_3.maxSlide = app.project_3.slides.length - 1,
+        app.project_4.maxSlide = app.project_4.slides.length - 1,
+
+        app.project_1.slides.forEach((slide, index) => {
+
+            // place the images end to end on the page
+            slide.style.transform = `translateX(${index * 100}%)`;
+        
+            // add animation property to the images
+            slide.style.animation = `fade-in 1s`;
+        
+            // once images have loaded, display them on the page
+            slide.style.display = `block`;
+        });
+
+        app.project_2.slides.forEach((slide, index) => {
+
+            // place the images end to end on the page
+            slide.style.transform = `translateX(${index * 100}%)`;
+        
+            // add animation property to the images
+            slide.style.animation = `fade-in 1s`;
+        
+            // once images have loaded, display them on the page
+            slide.style.display = `block`;
+        });
+
+        app.project_3.slides.forEach((slide, index) => {
+
+            // place the images end to end on the page
+            slide.style.transform = `translateX(${index * 100}%)`;
+        
+            // add animation property to the images
+            slide.style.animation = `fade-in 1s`;
+        
+            // once images have loaded, display them on the page
+            slide.style.display = `block`;
+        });
+
+        app.project_4.slides.forEach((slide, index) => {
 
             // place the images end to end on the page
             slide.style.transform = `translateX(${index * 100}%)`;
@@ -135,7 +208,10 @@ let app = {
         });
 
         // update slide text to current slide
-        app.functions.changeSlideText();
+        app.functions.changeSlideText(1);
+        app.functions.changeSlideText(2);
+        app.functions.changeSlideText(3);
+        app.functions.changeSlideText(4);
 
         // add the event listeners
         app.events();
